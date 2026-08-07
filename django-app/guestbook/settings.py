36e6c8395530a3b57113746af4340a9ce177030b
Reza <ghanbari.reza94@gmail.com>
2026-08-07 19:40:57 +0200

    Fix sqlite ENGINE path in Django settings

--- django-app/guestbook/settings.py
+++ django-app/guestbook/settings.py
@@ -36,7 +36,7 @@ TEMPLATES = [{
 }]
 WSGI_APPLICATION = "guestbook.wsgi.application"
 DATABASES = {"default": {
-    "ENGINE": "django.db.sqlite3",
+    "ENGINE": "django.db.backends.sqlite3",
     "NAME": os.environ.get("DJANGO_DB_PATH", BASE_DIR / "db.sqlite3"),
 }}
 STATIC_URL = "static/"
